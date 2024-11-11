006a348eecdd0c5d4f65f3bb2ef21dc969402ed7
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2024-11-11 21:46:00 +0700

    Update Character.h

--- finished-code/Character.h
+++ finished-code/Character.h
@@ -11,15 +11,12 @@ private:
     
 public:
 	static Character* createCharacter(CharacterType type);
-    void move();
-    void jump();
-    void die();
-<<<<<<< HEAD
-    void collect(WorldObject* object);
-    void standOn(WorldObject* object);
-    void setPositionFor(WorldObject* object);
-    void hit(WorldObject* object);
-=======
->>>>>>> b5f28a4db16f4dac39d4209019788812670ed3b0
+	void move();
+	void jump();
+	void die();
+	void collect(WorldObject* object);
+	void standOn(WorldObject* object);
+	void setPositionFor(WorldObject* object);
+	void hit(WorldObject* object);
 };
 
