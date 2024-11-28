7a63183e107d9a4fdbcbd188b11a84f2cd6de797
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2024-11-28 15:21:12 +0700

    Update Mario.cpp

--- VU_MARIO/Mario.cpp
+++ VU_MARIO/Mario.cpp
@@ -1,33 +1,13 @@
 #include "Mario.h"
 
-Mario::Mario(sf::Texture *texture, sf::Vector2f size, sf::Vector2f startingPos)
-{
-	this->xAccel = X_ACCEL;
-	this->xMax = X_MAX;
-	this->yInstSpeed = Y_INST_SPEED;
-	this->xDecel = X_DECEL;
-	this->state = PositionalState::GROUNDED;
-	this->Vx = 0;
-	this->Vy = 0;
-	
-	// this will need further texture loading later
-	
-	this->shape->setSize(size);
-	this->shape->setPosition(startingPos);
-	this->sprite->setTextureRect(sf::IntRect(0, 0, size.x, size.y)); // sets the default texture to the first one
-}
+Mario::Mario(){}
 
 void Mario::hit(NonPlayableCharacter* character) {
-	character->die(); // i guess?
+	//write
 }
 
-//void Mario::beingStoodOn(){
-//	this->die(); // i also guess?
-//}
-
-void Mario::collect(Item *item)
-{
-	// pending implementation
+void Mario::beingStoodOn(){
+	//write
 }
 
 void Mario::move(){
@@ -35,7 +15,7 @@ void Mario::move(){
 }
 
 
-void Mario::shoot()
-{
-	// pending implementation
-}
+	
+
+
+
