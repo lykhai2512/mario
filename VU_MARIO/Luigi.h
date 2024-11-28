532423c09f214f0be1baa3dda2758420c12ad514
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2024-11-28 15:20:11 +0700

    Update Luigi.h

--- VU_MARIO/Luigi.h
+++ VU_MARIO/Luigi.h
@@ -2,22 +2,9 @@
 #include "PlayableCharacter.h"
 
 class Luigi : public PlayableCharacter{
-private:
-	const float X_ACCEL = 3, X_MAX = 15;
-	const float X_DECEL = 2.5;
-	const float Y_INST_SPEED = 40;
-public:
 
+public:
 	Luigi();
 	void move();
-
-	Luigi(sf::Texture* texture, sf::Vector2f size, sf::Vector2f startingPos);
-	void hit(NonPlayableCharacter* character) override;
-	//void beingStoodOn();
-	void collect(Item* item) override;
-	void standOn(NonPlayableCharacter* character) override;
-	void shoot() override;
-	// move/jump inherits playableChar.h
-
 };
 
